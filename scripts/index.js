a7c8e638af1d9e32b60edeb8afaf27b0f6938fde
Haley Guilfoile <koala.la.love@gmail.com>
2017-09-06 15:07:04 -0500

    ummm redid entire website oops

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-$('.icon-control').click(function(){
-	$('.icon-control').removeClass('active');
-	$(this).toggleClass('active');
-  var selector = '#' + $(this).data('target');
-  $('.paragraph').hide();
-  $(selector).toggle();
-});
-
-$('#first-icon').click();
